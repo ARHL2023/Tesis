f1e855d562dcb5f2bbb00ab22e8180d1a65b8515
ARHL2023 <129900679+ARHL2023@users.noreply.github.com>
2023-06-07 11:53:29 -0500

    add controllrt

--- TEKA/src/main/java/TEKA/controllers/LoginController.java
+++ TEKA/src/main/java/TEKA/controllers/LoginController.java
@@ -1,7 +1,14 @@
 package TEKA.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
+
+    @GetMapping(value = "value")
+        public String loginPage(){
+            return "valor";
+    }
+
 }
